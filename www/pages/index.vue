81656367b63713378363feba81ecfa9794c3c349
zhu.jun <aubrey.jun@outlook.com>
2023-12-09 13:14:27 +0800

    增加使用naviui. Signed-off-by: zhu.jun <aubrey.jun@outlook.com>

--- www/pages/index.vue
+++ www/pages/index.vue
@@ -1,48 +1,33 @@
 <template>
   <div class="container_12">
-    <div class="block text-center" style="height: 300px">
-      <el-carousel height="auto" autoplay>
-        <el-carousel-item style="height: 300px">
-          <h3 class="small justify-center" text="2xl">height 100px</h3>
-          <el-image  src="static/images/banner_1.jpg" fit="fill" />
-        </el-carousel-item>
-        <el-carousel-item style="height: 300px">
-          <el-image  src="static/images/banner_2.jpg" fit="fill" />
-          <h3 class="small justify-center" text="2xl">height 200px</h3>
-        </el-carousel-item>
-        <el-carousel-item style="height: 300px">
-          <el-image  src="static/images/banner_1.jpg" fit="fill" />
-          <h3 class="small justify-center" text="2xl">height 300px</h3>
-        </el-carousel-item>
-      </el-carousel>
-    </div>
+    <n-carousel show-arrow>
+      <img
+          class="carousel-img"
+          src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg"
+      >
+      <img
+          class="carousel-img"
+          src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg"
+      >
+      <img
+          class="carousel-img"
+          src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg"
+      >
+      <img
+          class="carousel-img"
+          src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg"
+      >
+    </n-carousel>
   </div>
 </template>
 <style scoped>
-.carousel-item {
-  color: #475669;
-  opacity: 0.75;
-  margin: 0;
-  text-align: center;
-}
-
-.el-carousel__item h3 {
-  color: #475669;
-  opacity: 0.75;
-  display: flex;
-  align-items: center;
-  margin: 0;
-  text-align: center;
-  height: 100%;
-}
-
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+.carousel-img {
+  width: 100%;
+  height: 240px;
+  object-fit: cover;
 }
 </style>
+
 <script setup lang="ts">
+import {NCarousel} from "naive-ui";
 </script>
